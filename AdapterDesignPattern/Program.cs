ec381ad07d7ba70642ba11f6001e1e5e61d0f89a
Ravikumar Makwana <17comp.ravi.makwana@gmail.com>
2023-02-10 09:32:32 +0530

    Implement Prototype and Adapter Design Pattern

---
+++ AdapterDesignPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        IEmployee employee = new EmployeeAdapter();
+        Console.WriteLine(employee.GetAllEmployees());
+    }
+}
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Salary { get; set; }
+}
+
+public interface IEmployee
+{
+    string GetAllEmployees();
+}
+
+public class EmployeeManager
+{
+    public List<Employee> Employees { get; set; }
+
+    public EmployeeManager()
+    {
+        Employees = new List<Employee>
+        {
+            new Employee { Id = 1, Name = "Ram", Salary = 1234 },
+            new Employee { Id = 2, Name = "Raj", Salary = 2345 },
+            new Employee { Id = 3, Name = "Alice", Salary = 3456 }
+        };
+    }
+
+    // Format Employees: [ "Ram", "Raj", "Alice"]
+    public virtual string GetAllEmployees()
+    {
+        return $"Employees : [{string.Join(", ", Employees.Select(s => s.Name))}]";
+    }
+}
+
+public class EmployeeAdapter : EmployeeManager, IEmployee
+{
+    // Format Employees: [ "Ram's Salary: 1234", "Raj's Salary: 2345", "Alice's Salary: 3456"]
+    public override string GetAllEmployees()
+    {
+        return $"Employees: [{string.Join(", ", Employees.Select(s => $"{s.Name}'s Salary: {s.Salary}"))}]";
+    }
+}
---
+++ PrototypeDesignPattern/Program.cs
@@ -0,0 +1,62 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var car1 = new Car("I6", "BMW", new Feature { Color = "Red", Weight = 400, MaxSpeed = 100 });
+        var car2 = car1.Clone();
+
+        car1.Model = "I9";
+        car1.Feature.Color = "Black";
+
+        Console.WriteLine(car1.ToString());
+        Console.WriteLine(car2.ToString());
+    }
+}
+
+public abstract class Prototype
+{
+    public abstract Prototype Clone();
+}
+
+public class Feature
+{
+    public string Color { get; set; }
+    public int Weight { get; set; }
+    public int MaxSpeed { get; set; }
+}
+
+public class Car : Prototype
+{
+    public string Model { get; set; }
+    public string Brand { get; set; }
+    public Feature Feature { get; set; }
+
+    public Car(string model, string brand, Feature feature)
+    {
+        Model = model;
+        Brand = brand;
+        Feature = feature;
+    }
+
+    public override string ToString()
+    {
+        return $"Mode: {Model}, Brand: {Brand}, Features: [Color: {Feature.Color}, Weight: {Feature.Weight}, MaxSpeed: {Feature.MaxSpeed}]";
+    }
+
+    // Shallow Copy
+    // public override Car Clone()
+    // {
+    //     return new Car(Model, Brand, Feature);
+    // }
+
+    // Deep Copy
+    public override Car Clone()
+    {
+        return new Car(Model, Brand, new Feature
+        {
+            Color = Feature.Color,
+            MaxSpeed = Feature.MaxSpeed,
+            Weight = Feature.Weight
+        });
+    }
+}
